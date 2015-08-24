0096945c7bc9ad97742d3827d7bdeb9c236957d1
paulsumm <paulsumm@microsoft.com>
2015-08-24 17:33:15 -0400

    Initial commit

---
+++ pjsummersjr.O365.Samples.Navigation/NavigationManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+
+using Microsoft.SharePoint.Client;
+
+namespace pjsummersjr.O365.Samples.Navigation
+{
+    class NavigationManager
+    {
+        private ClientContext spClient;
+        private SharePointOnlineCredentials creds;
+        public string SiteUrl { get; set; }
+        public string UserName { get; set; }
+        public System.Security.SecureString Password { get; set; }
+
+        public NavigationManager(string siteUrl, string userName, System.Security.SecureString password)
+        {
+            SiteUrl = siteUrl;
+            UserName = userName;
+            Password = password;
+        }
+
+        public NavigationNodeCollection GetTopNavigation()
+        {
+            using (var ctx = GetSPContext())
+            {
+                NavigationNodeCollection navNodes = ctx.Web.Navigation.TopNavigationBar;
+                ctx.Load(navNodes);
+                ctx.ExecuteQuery();
+                return navNodes;
+            }
+        }
+
+        public NavigationNodeCollection AddNodeToNavigation(NavigationNodeCreationInformation node)
+        {
+            NavigationNodeCollection navNodes = GetTopNavigation();
+            navNodes.Add(node);
+            using (var ctx = GetSPContext())
+            {
+                ctx.Load(navNodes);
+                ctx.ExecuteQuery();
+            }
+            return navNodes;
+        }
+
+        public NavigationNodeCollection RemoveNodeByTitle(string nodeTitle)
+        {
+            NavigationNodeCollection navNodes = GetTopNavigation();
+            var deleteNodes = from node in navNodes where node.Title.Equals(nodeTitle) select node;
+            var nodeToDelete = deleteNodes.First();
+            nodeToDelete.DeleteObject();
+
+            using (var ctx = GetSPContext())
+            {
+                ctx.Load(navNodes);
+                ctx.ExecuteQuery();
+            }
+
+            return navNodes;
+        }
+
+        public NavigationNodeCollection AddNodeToNavigation(string Title, string link)
+        {
+            NavigationNodeCreationInformation navNodeDescriptor = new NavigationNodeCreationInformation();
+            navNodeDescriptor.Title = Title;
+            navNodeDescriptor.Url = link;
+            return AddNodeToNavigation(navNodeDescriptor);
+        }
+
+        private SharePointOnlineCredentials GetCredentials()
+        {
+            if (creds == null)
+            {
+                creds = new SharePointOnlineCredentials(UserName, Password);
+            }
+            return creds;
+        }
+
+        private ClientContext GetSPContext()
+        {
+            if (spClient == null)
+            {
+                spClient = new ClientContext(SiteUrl);
+                spClient.Credentials = GetCredentials();
+            }
+
+            return spClient;
+        }
+    }
+}
---
+++ pjsummersjr.O365.Samples.Navigation/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Security;
+
+using Microsoft.SharePoint.Client;
+
+namespace pjsummersjr.O365.Samples.Navigation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "admin@paulsumm0715.onmicrosoft.com";
+            string password = "pass@word1";
+            string siteUrl = "https://paulsumm0715.sharepoint.com/sites/llbean";
+
+            NavigationManager navMan = new NavigationManager(siteUrl, username, GetStringAsSecureString(password));
+
+            Console.WriteLine("***************** Current Navigation **********************");
+            NavigationNodeCollection navNodes = navMan.GetTopNavigation();
+            foreach (var node in navNodes)
+            {
+                Console.WriteLine(node.Title);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("***************** Adding node - Title: \"Paul's Subsite\" *****************");
+            navMan.AddNodeToNavigation("Paul's Subsite", "http://www.microsoft.com");
+            Console.WriteLine("Added node successfully.");
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("***************** Print Updated Navigation **********************");
+            NavigationNodeCollection newNavNodes = navMan.GetTopNavigation();
+            foreach (var node in navNodes)
+            {
+                Console.WriteLine(node.Title);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("***************** Removing nodes containing - Title: \"Paul's Subsite\" *****************");
+            navMan.RemoveNodeByTitle("Paul's Subsite");
+            Console.WriteLine("Removed nodes successfully.");
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("***************** Print Updated Navigation **********************");
+            newNavNodes = navMan.GetTopNavigation();
+            foreach (var node in navNodes)
+            {
+                Console.WriteLine(node.Title);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("Demo complete. Press any key to quit...");
+            Console.ReadLine();
+
+
+        }
+
+        private static SecureString GetStringAsSecureString(string str)
+        {
+            SecureString secStr = new SecureString();
+            foreach (char c in str)
+            {
+                secStr.AppendChar(c);
+            }
+
+            return secStr;
+        }
+    }
+}

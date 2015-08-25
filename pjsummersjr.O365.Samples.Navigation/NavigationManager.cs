9117f60b75d7802e35483a5ad912d8698a36154f
paulsumm <paulsumm@microsoft.com>
2015-08-25 13:14:50 -0400

    Documentation

--- pjsummersjr.O365.Samples.Navigation/NavigationManager.cs
+++ pjsummersjr.O365.Samples.Navigation/NavigationManager.cs
@@ -4,12 +4,24 @@ using Microsoft.SharePoint.Client;
 
 namespace pjsummersjr.O365.Samples.Navigation
 {
+    /// <summary>
+    /// This class abstracts many of the operations required for managing navigation in a SharePoint Online site
+    /// </summary>
     class NavigationManager
     {
         private ClientContext spClient;
         private SharePointOnlineCredentials creds;
+        /// <summary>
+        /// The SharePoint site where the navigation will be managed
+        /// </summary>
         public string SiteUrl { get; set; }
+        /// <summary>
+        /// The username of a user with appropriate permissions for managing the navigation
+        /// </summary>
         public string UserName { get; set; }
+        /// <summary>
+        /// The password of the above user
+        /// </summary>
         public System.Security.SecureString Password { get; set; }
 
         public NavigationManager(string siteUrl, string userName, System.Security.SecureString password)
@@ -18,7 +30,10 @@ namespace pjsummersjr.O365.Samples.Navigation
             UserName = userName;
             Password = password;
         }
-
+        /// <summary>
+        /// Returns the NavigationNodeCollection for the top navigation, also known as the Global Navigation
+        /// </summary>
+        /// <returns></returns>
         public NavigationNodeCollection GetTopNavigation()
         {
             using (var ctx = GetSPContext())
@@ -29,7 +44,11 @@ namespace pjsummersjr.O365.Samples.Navigation
                 return navNodes;
             }
         }
-
+        /// <summary>
+        /// Adds a node to the navigation
+        /// </summary>
+        /// <param name="node">The parameters for the new navigation node encapsulated in a NavigationNodeCreationInformation object</param>
+        /// <returns></returns>
         public NavigationNodeCollection AddNodeToNavigation(NavigationNodeCreationInformation node)
         {
             NavigationNodeCollection navNodes = GetTopNavigation();
@@ -41,7 +60,26 @@ namespace pjsummersjr.O365.Samples.Navigation
             }
             return navNodes;
         }
-
+        /// <summary>
+        /// Adds a node to the navigation. This only requires the display value (Title) and link that the NavigationNode will
+        /// follow.
+        /// </summary>
+        /// <param name="Title">Display value for the NavigationNode</param>
+        /// <param name="link">URL to which the NavigationNode will link</param>
+        /// <returns></returns>
+        public NavigationNodeCollection AddNodeToNavigation(string Title, string link)
+        {
+            NavigationNodeCreationInformation navNodeDescriptor = new NavigationNodeCreationInformation();
+            navNodeDescriptor.Title = Title;
+            navNodeDescriptor.Url = link;
+            return AddNodeToNavigation(navNodeDescriptor);
+        }
+        /// <summary>
+        /// Removes a node from the navigation with the specified title. If multiple nodes have the same title, only the first will
+        /// be removed so the command will have to be run multiple times. Requires an exact match.
+        /// </summary>
+        /// <param name="nodeTitle"></param>
+        /// <returns></returns>
         public NavigationNodeCollection RemoveNodeByTitle(string nodeTitle)
         {
             NavigationNodeCollection navNodes = GetTopNavigation();
@@ -56,14 +94,6 @@ namespace pjsummersjr.O365.Samples.Navigation
             }
 
             return navNodes;
-        }
-
-        public NavigationNodeCollection AddNodeToNavigation(string Title, string link)
-        {
-            NavigationNodeCreationInformation navNodeDescriptor = new NavigationNodeCreationInformation();
-            navNodeDescriptor.Title = Title;
-            navNodeDescriptor.Url = link;
-            return AddNodeToNavigation(navNodeDescriptor);
         }
 
         private SharePointOnlineCredentials GetCredentials()
